2a592189f67d920a3c9692bb485cfdf7d9adf8a8
Jaxongir <jorajaxongir229@gmail.com>
2025-05-07 22:20:15 +0500

    Travel-site 4

--- apps/service/views.py
+++ apps/service/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from . models import Service,FeedbackClient
+from django  import template
 
+
+register = template.Library()
+
+@register.filter
+def times(number):
+    return range(number)
 
 def serviceView(request):
-    return render(request, 'services.html')
+    return render(request, 'services.html',context={
+        'service1': Service.objects.all()[:1],
+        'service2': Service.objects.all()[1:],
+        'feedback_clients': FeedbackClient.objects.all(),
+        })
