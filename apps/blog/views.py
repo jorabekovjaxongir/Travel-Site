82bdbfcfd7e4c0dfa86ae15734ba3fedef2f8e41
Jaxongir <jorajaxongir229@gmail.com>
2025-05-27 16:23:07 +0500

    Travel-Site 6

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Blog
+from apps.package.forms import SubscriptionForm
 
 
 def blogView(request):
-    return render(request, 'blog.html')
+    subs = SubscriptionForm(request.POST or None, initial={'persons': 'ONE', 'kids': 1})
+    blog = Blog.objects.all().order_by('-id')
+    return render(request, 'blog.html', {
+        'blogs': blog,
+        "subs": subs,
+
+    })
